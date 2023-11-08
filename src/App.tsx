376ca898ac28afb2553aa89c8bdb422334323465
walter lacoste <wlacoste@outlook.com>
2023-11-08 01:55:41 -0300

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div>Seccion1</div>
+      <div>Seccion2</div>
+      <div>Seccion3</div>
+      <div>Seccion4</div>
+      <div>Footer</div>
+    </>
+  );
+}
+
+export default App;
